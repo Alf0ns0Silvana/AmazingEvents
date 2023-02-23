e761aa1c680b66b0459425a56ed72359dd846d97
Silvana Alfonso <alfonsosilvana16@gmail.com>
2023-02-23 14:18:10 -0300

    task2js cards modif

---
+++ upcomingevents.js
@@ -0,0 +1,21 @@
+let htmlEvents = "";
+let cardContainer = document.getElementById("cardContainer");
+
+let currentDate = new Date(data.currentDate);
+for (let event of data.events) {
+    let eventDate = new Date(event.date);
+    if (eventDate > currentDate) {
+        htmlEvents += `<div class="card d-flex text-center border-secondary" style="width: 15rem;">
+            <img src="${event.image}" class="card-img-top">
+            <div class="card-body">
+                <h3 class="card-title mb-3">${event.name}</h3>
+                <p class="card-text mb-3">${event.description}</p>
+            </div>
+              <div class="d-flex justify-content-center align-items-center align-self-bottom mb-3">
+                <span>Price: $ ${event.price}</span>
+                  <a href="./event.html" class="btn btn-outline-secondary me-md-2" id=${event._id} onClick="detailCard(this.id)">More</a>
+              </div>
+            </div>`;
+        }
+console.log(htmlEvents)
+}
