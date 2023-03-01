972ad933defd9122b98ff3d04cdf047734be7ed3
Silvana Alfonso <alfonsosilvana16@gmail.com>
2023-03-01 12:43:19 -0300

    fixed function cards

--- data.js
+++ data.js
@@ -171,3 +171,17 @@ let data = {
     }
   ]
 };
+function createCard(event) {
+  let card =  `<div class="card d-flex text-center border-secondary" style="width: 15rem;">
+  <img src="${event.image}" class="card-img-top">
+  <div class="card-body">
+      <h3 class="card-title mb-3">${event.name}</h3>
+      <p class="card-text mb-3">${event.description}</p>
+  </div>
+    <div class="d-flex justify-content-center align-items-center align-self-bottom mb-3">
+      <span>Price: $ ${event.price}</span>
+        <a href="./event.html" class="btn btn-outline-secondary me-md-2" id=${event._id} onClick="detailCard(this.id)">More</a>
+    </div>
+  </div>`
+  return card;
+};
